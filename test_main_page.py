6da726d47de1564aed721a2378de8337a8b396a0
Oleksandr <akurylo85@gmail.com>
2022-09-24 13:36:20 +0200

    add locators file for move selector to external variable

--- test_main_page.py
+++ test_main_page.py
@@ -12,11 +12,3 @@ def test_guest_should_see_login_link(browser):
     page.open()
     page.should_be_login_link()
 
-
-#from selenium.webdriver.common.by import By
-
-#def test_guest_can_go_to_login_page(browser):
-#    link = "http://selenium1py.pythonanywhere.com/"
-#    browser.get(link)
-#    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-#    login_link.click()
