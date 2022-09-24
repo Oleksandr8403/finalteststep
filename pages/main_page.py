bb1dca021112418d54608a5a9e054a3d02744176
Oleksandr <akurylo85@gmail.com>
2022-09-24 12:49:12 +0200

    add method should_be_login_link

--- pages/main_page.py
+++ pages/main_page.py
@@ -5,3 +5,6 @@ class MainPage(BasePage):
     def go_to_login_page(self):
         login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
         login_link.click()
+
+    def should_be_login_link(self):
+        self.browser.find_element(By.CSS_SELECTOR, "#login_link_invalid")
--- test_main_page.py
+++ test_main_page.py
@@ -6,6 +6,12 @@ def test_guest_can_go_to_login_page(browser):
     page.open()                      # открываем страницу
     page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
 
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
 
 #from selenium.webdriver.common.by import By
 
