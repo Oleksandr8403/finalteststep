3a5cd09e5c75eb9e1b3ca7554ab6657df1d157cc
Oleksandr <akurylo85@gmail.com>
2022-09-30 18:25:31 +0200

    delete one test, that not need for course

--- test_product_page.py
+++ test_product_page.py
@@ -1,12 +1,6 @@
 from .pages.main_page import MainPage
 from .pages.product_page import PageProduct
 
-def test_guest_should_see_basket_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-    page = MainPage(browser, link)
-    page.open()
-    page.should_be_basket_link()
-
 def test_guest_can_add_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
     page = MainPage(browser, link)
