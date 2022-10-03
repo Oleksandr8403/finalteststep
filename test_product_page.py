233fb3ba6cb021ab3357a0bf8a74c6a435a71ee1
Oleksandr <akurylo85@gmail.com>
2022-10-03 14:48:38 +0200

    move three tests from test_negative_checks to test_product_page and delete test_negative_checks.py file

--- test_product_page.py
+++ test_product_page.py
@@ -76,3 +76,30 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     basket_page_from_items_page = BasketPage(browser, browser.current_url)
     basket_page_from_items_page.should_basket_without_items()
     basket_page_from_items_page.should_basket_message_without_items_is_present()
+
+@pytest.mark.xfail(reason="we know that this test is fail")
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+    page = MainPage(browser, link)
+    page.open()
+    page.click_add_to_basket()
+    page.solve_quiz_and_get_code()
+    after_click_basket_page = PageProduct(browser, browser.current_url)
+    after_click_basket_page.should_not_be_success_message()
+
+def test_guest_cant_see_success_message(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+    page = MainPage(browser, link)
+    page.open()
+    without_clock_basket = PageProduct(browser, browser.current_url)
+    without_clock_basket.should_not_be_success_message()
+
+@pytest.mark.xfail(reason="we know that this test is fail")
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+    page = MainPage(browser, link)
+    page.open()
+    page.click_add_to_basket()
+    page.solve_quiz_and_get_code()
+    after_click_basket_page = PageProduct(browser, browser.current_url)
+    after_click_basket_page.should_success_message_is_disappeared()
