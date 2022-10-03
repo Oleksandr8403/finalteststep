154c1a6104a8dccc41aa3184b96c805039a9e2f7
Oleksandr <akurylo85@gmail.com>
2022-10-03 17:40:37 +0200

    preparing the project for code review, step 1

--- pages/locators.py
+++ pages/locators.py
@@ -17,7 +17,7 @@ class ProductPageLocators():
     NAME_BOOK = (By.CSS_SELECTOR, "div.col-sm-6.product_main > h1")
     ALERT_BOOK = (By.CSS_SELECTOR, "#messages > div:nth-child(1) > div > strong")
     PRICE_BOOK = (By.CSS_SELECTOR, "div.col-sm-6.product_main > p.price_color")
-    ALERT_PRICE_BOOK = (By.CSS_SELECTOR, "div.alert.alert-safe.alert-noicon.alert-info.fade.in > div > p:nth-child(1) > strong")
+    ALERT_PRICE_BOOK = (By.CSS_SELECTOR, "div.alert> div > p:nth-child(1) > strong")
     SUCCESS_MESSAGE = (By.CSS_SELECTOR, "#messages > div:nth-child(2) > div > strong")
 
 class BasePageLocators():
--- test_product_page.py
+++ test_product_page.py
@@ -25,6 +25,7 @@ class TestUserAddToBasketFromProductPage:
         without_click_basket = PageProduct(browser, browser.current_url)
         without_click_basket.should_not_be_success_message()
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
         page = MainPage(browser, link)
@@ -36,7 +37,7 @@ class TestUserAddToBasketFromProductPage:
         after_click_basket_page.should_be_price_correct()
 
 
-
+@pytest.mark.need_review
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
@@ -62,12 +63,14 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.open()
     page.should_be_login_link()
 
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
 
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = PageProduct(browser, link)
